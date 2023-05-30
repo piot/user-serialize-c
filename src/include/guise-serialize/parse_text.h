c1483eec1f711bf004770b5d50003a08c03d0147
Peter Bjorklund <piot@hotmail.com>
2023-05-30 16:09:36 +0200

    chore: missing header

--- src/include/guise-serialize/parse_text.h
+++ src/include/guise-serialize/parse_text.h
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 #ifndef GUISE_SERIALIZE_PARSE_TEXT_H
 #define GUISE_SERIALIZE_PARSE_TEXT_H
 
--- src/lib/parse_text.c
+++ src/lib/parse_text.c
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 #include <clog/clog.h>
 #include <flood/in_stream.h>
 #include <flood/text_in_stream.h>
